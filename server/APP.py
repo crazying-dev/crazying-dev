6a9c15e3dada96ab1089b9fded035113114af0a0
crazying-dev <3890320020@qq.com>
2026-05-01 17:01:23 +0800

    test

--- server/APP.py
+++
@@ -1,3 +0,0 @@
-from flask import *
-
-app = Flask(__name__)
--- server/main.py
+++ server/main.py
@@ -1,7 +1,39 @@
-from GET import app, render_template, abort, send_file, request
-from tool import *
+import os
+import random
+
+from flask import *
+
+app = Flask(__name__)
+
 
 base = 'path/base.html'
+
+# tool.py
+def get_random_file(dir_path):
+	# 收集所有文件
+	files = []
+	for name in os.listdir(dir_path):
+		fp = os.path.join(dir_path, name)
+		if os.path.isfile(fp):
+			files.append(fp)
+	if not files:
+		return None
+	return os.path.abspath(os.path.normpath(random.choice(files)))
+
+# tool.py end
+
+
+
+#GET.py
+@app.route('/GET')
+def indexGet():
+	return render_template('index.html')
+
+
+@app.route('/AboutMe/GET')
+def AboutMeGet():
+	return render_template('HTML/AboutMe.html')
+
 
 
 @app.route('/')
@@ -33,7 +65,6 @@ def bg():
 		return send_file(file, mimetype='image/webg')
 	abort(404)
 """
-
 
 if __name__ == '__main__':
 	app.run(debug=True)
