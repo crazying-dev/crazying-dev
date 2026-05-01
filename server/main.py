f51230f9b79b594d70f013ccae6d08647bfd214a
crazying-dev <3890320020@qq.com>
2026-05-01 21:58:59 +0800

    add bg

--- server/main.py
+++ server/main.py
@@ -47,24 +47,24 @@ def AboutMe():
 	return render_template(base)
 
 
-"""
+
 @app.route('/bg')
 def bg():
 	Type = request.args.get('type', None)
 	if (Type is None) or (Type not in ['pc', 'mobile']):
 		return abort(404)
 	if Type == 'pc':
-		file = get_random_file('./static/img/bg/pc')
+		file = get_random_file('/static/img/bg/pc')
 		if file is None:
 			return abort(404)
 		return send_file(file, mimetype='image/webg')
 	elif Type == 'mobile':
-		file = get_random_file('./static/img/bg/mobile')
+		file = get_random_file('/static/img/bg/mobile')
 		if file is None:
 			return abort(404)
 		return send_file(file, mimetype='image/webg')
 	abort(404)
-"""
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
