b15c1b6f942699be9cacc4df5634bf2f530f3121
crazying-dev <3890320020@qq.com>
2026-05-02 08:42:52 +0800

    add 404

--- server/main.py
+++ server/main.py
@@ -1,12 +1,15 @@
 import os
 import random
+import json
 
 from flask import *
 
 app = Flask(__name__)
 
-
 base = 'path/base.html'
+webside = 'www.crazying-dev.top'
+
+f = open('posts.json','r')
 
 # tool.py
 def get_random_file(dir_path):
@@ -34,6 +37,27 @@ def indexGet():
 def AboutMeGet():
 	return render_template('HTML/AboutMe.html')
 
+@app.route('/post/<int:id>/GET')
+def postGet(id):
+	Type = request.args.get('type', None)
+	with open(f'post/{id}.json','r') as _f:
+		info = json.loads(_f.read())
+	if Type == 'avatar':
+		return info['avatar']
+	elif Type == 'message':
+		return info['message']
+	elif Type == 'data':
+		return info['data']
+	elif Type == 'desc':
+		return info['desc']
+	elif Type == 'title':
+		return info['title']
+	elif Type is None:
+		return render_template('/path/post.html')
+	else:
+		return abort(400)
+		
+#GET.py end
 
 
 @app.route('/')
@@ -52,19 +76,51 @@ def AboutMe():
 def bg():
 	Type = request.args.get('type', None)
 	if (Type is None) or (Type not in ['pc', 'mobile']):
-		return abort(404)
+		return abort(400)
 	if Type == 'pc':
 		file = get_random_file('/static/img/bg/pc')
 		if file is None:
-			return abort(404)
+			return abort(500)
 		return send_file(file, mimetype='image/webg')
 	elif Type == 'mobile':
 		file = get_random_file('/static/img/bg/mobile')
 		if file is None:
-			return abort(404)
+			return abort(500)
 		return send_file(file, mimetype='image/webg')
-	abort(404)
+	abort(400)
 
+@app.route('/rss.xml')
+def rss():
+	posts = json.loads(f.read().replace('<;;;>',f'https://{webside}'))
+	
+
+	xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+	xml += '<rss version="2.0">\n<channel>\n'
+	xml += f'<title>我的站点</title>\n<link>{webside}</link>\n<description>个人博客</description>\n'
+
+	for p in posts:
+		xml += f"""
+<item>
+  <title>{p['title']}</title>
+  <link>{p['link']}</link>
+  <description>{p['desc']}</description>
+  <pubDate>{p['date']}</pubDate>
+</item>
+"""
+	xml += '</channel>\n</rss>'
+
+	res = make_response(xml)
+	res.headers["Content-Type"] = "application/xml"
+	return res
+
+@app.route('/post/<int:id>')
+def post(id):
+	return render_template(base)
+
+	
+@app.errorhandler(404)
+def not_found():
+	return render_template('error/404.html'),404
 
 if __name__ == '__main__':
 	app.run(debug=True)
